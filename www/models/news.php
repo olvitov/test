e4d4f20dd3c4debe444da91ba417505cd4896c91
olvit <olvitov2013@gmail.com>
2016-05-05 12:08:15 +0300

    Модель

---
+++ www/models/news.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../classes/DB.php';
+
+class News {
+    public $id;
+    public $title;
+    public $text;
+
+  public  static function getAll() {
+
+        $db = new DB;
+        return $db->query('SELECT * FROM news', 'News');
+    }
+
+
+}
+
